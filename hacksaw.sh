45800408497bec25bb0c29c0a9182f0156154d2b
Dave Parfitt <diparfitt@gmail.com>
2011-12-14 08:17:12 -0500

    removed hard coded path

--- hacksaw.sh
+++ hacksaw.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # JUST for the demo...
-export HACKSAW_HOME=/Users/dparfitt/src/hacksaw
-export JAVA_OPTS=-javaagent:${HACKSAW_HOME}/Hacksaw.jar
+#export HACKSAW_HOME=/Users/dparfitt/src/hacksaw
+export JAVA_OPTS=-javaagent:Hacksaw.jar
 jruby.sh $1
