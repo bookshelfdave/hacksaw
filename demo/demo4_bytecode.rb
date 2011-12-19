04830d38943c76ec7e293583eb3f3378a6fb440d
Dave Parfitt <diparfitt@gmail.com>
2011-12-19 08:55:49 -0500

    One more comment before I forget.

--- demo/demo4_bytecode.rb
+++ demo/demo4_bytecode.rb
@@ -20,6 +20,9 @@ modify :classes=>/com.quadcs.hacksaw.demo.DemoAccount/ do |c|
 
         # this replaces ALL exceptions thrown with a NOP *and* a POP      
  	# this needs some work
+ 	
+ 	# need to come up with a decent syntax to represent Bytecode ops
+ 	# such as inserting code around the current instruction etc.
         m.map_bytecode do |bytes,op|         
           op == "athrow" ? 0 : bytes
 	end
